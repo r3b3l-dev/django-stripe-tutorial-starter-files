3f98e66d3dfac0115012224c27f1e545e364d3ae
Wayne <wayne.manning@tuta.io>
2022-06-09 10:38:51 +0100

    Add product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.Charfield(max_length=100)
+    price = models.Integerfield(default=0) # cents
+
+    def __str__(self):
+        return self.name
+
