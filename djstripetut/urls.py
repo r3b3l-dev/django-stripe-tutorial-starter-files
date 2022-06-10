8c6c7e3a8f71abee699ffb80813c963e9d83cc28
Wayne <wayne.manning@tuta.io>
2022-06-10 09:47:09 +0100

    Add favicon and style landing page

--- djstripetut/urls.py
+++ djstripetut/urls.py
@@ -4,9 +4,15 @@ from products.views import (
     CreateCheckoutSessionView,
     ProductLandingPageView
 )
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', ProductLandingPageView.as_view(), name='landing-page'),
     path('create-checkout-session/', CreateCheckoutSessionView.as_view(), name='create-checout-session'),
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
